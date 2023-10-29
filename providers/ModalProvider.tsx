9a719b9d4f7a442b4ab4e0664d2456456d060590
Asilbek Norboev <asilbeknorboev@gmail.com>
2023-10-29 14:06:31 -0500

    integrated stripe: now it can succesfully subscribe to premium

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -6,8 +6,15 @@ import { useEffect, useState } from "react";
 import Modal from "@/components/Modal";
 import AuthModal from "@/components/AuthModal";
 import UploadModal from "@/components/UploadModal";
+import SubscribeModal from "@/components/SubscribeModal";
+import { ProductWithPrice } from "@/types";
 
-const ModalProvider = () => {
+interface ModalProviderProps{
+    products: ProductWithPrice[];
+}
+const ModalProvider:React.FC<ModalProviderProps> = ({
+    products
+}) => {
     const [isMounted , setIsMounted] = useState(false);
 
     useEffect(() => {
@@ -22,6 +29,7 @@ const ModalProvider = () => {
         <>
         <AuthModal/>
         <UploadModal/>
+        <SubscribeModal products={products}/>
         </>
     )
 }
